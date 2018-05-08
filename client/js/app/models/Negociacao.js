994048816592f4b21962743655d096fa94f13767
João Raul <raullmeneses@hotmail.com>
2018-05-08 09:17:13 -0300

    Finalizada aula 5 do Javascript avançado III

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -27,4 +27,8 @@ class Negociacao {
         
         return this._valor;
     }
+
+    isEquals(outraNegociacao){
+        return JSON.stringify(this) == JSON.stringify(outraNegociacao);
+    }
 }
